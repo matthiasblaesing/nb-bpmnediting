643fe1b91c9a4ea66de92df188de7a53c2b127df
Matthias Bläsing <mblaesing@doppel-helix.eu>
2021-11-27 23:42:59 +0100

    Add license headers

--- src/main/java/eu/doppelhelix/netbeans/plugin/bpmnediting/schema/BpmnSchemaProvider.java
+++ src/main/java/eu/doppelhelix/netbeans/plugin/bpmnediting/schema/BpmnSchemaProvider.java
@@ -1,9 +1,22 @@
+/*
+ * Copyright 2020 Matthias Bläsing.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package eu.doppelhelix.netbeans.plugin.bpmnediting.schema;
 
-import java.awt.Image;
 import java.beans.PropertyChangeListener;
-import java.net.URL;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.logging.Logger;
@@ -15,7 +28,7 @@ import org.openide.util.lookup.ServiceProvider;
 @ServiceProvider(service=CatalogReader.class, path="Plugins/XML/UserCatalogs")
 public class BpmnSchemaProvider implements CatalogReader, CatalogDescriptor2 {
     private static final Logger LOG = Logger.getLogger(BpmnSchemaProvider.class.getName());
-   
+
     @Override
     public Iterator getPublicIDs() {
         return Collections.EMPTY_LIST.iterator();
@@ -103,5 +116,5 @@ public class BpmnSchemaProvider implements CatalogReader, CatalogDescriptor2 {
     @Override
     public void removePropertyChangeListener(PropertyChangeListener pl) {
     }
-    
+
 }
